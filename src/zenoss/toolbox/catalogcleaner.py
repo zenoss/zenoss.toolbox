1f281ebf84c5091796e5069cfccd48f44ac2fb00
smousa <smousa@zenoss.com>
2013-08-13 14:43:45 -0500

    Added script to clean the catalog

---
+++ src/zenoss/toolbox/catalogcleaner.py
@@ -0,0 +1,26 @@
+import logging
+
+import Globals
+from Products.ZenUtils.ZenScriptBase import ZenScriptBase
+
+log = logging.getLogger("cleancatalog")
+
+def cleanZodb(dmd):
+    global_catalog = dmd.zport.global_catalog
+
+    for brain in global_catalog():
+        try:
+            brain.getObject()
+        except Exception:
+            log.warn("Found unbrainable path, deleting: %s", brain.getPath())
+            global_catalog.uncatalog_object(brain.getPath())
+
+    log.info("Finished scanning catalog")
+    
+def main(): 
+    dmd = ZenScriptBase(connect=True).dmd
+    cleanZodb(dmd)
+
+if __name__ == "__main__":
+    main()
+
